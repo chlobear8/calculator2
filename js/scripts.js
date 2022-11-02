8d0d583d49bf729093b1a616544f48b0a5c5f4c0
Chloe ONeil <chloesporri@yahoo.com>
2022-11-01 20:14:46 -0700

    add body with label and input

---
+++ js/scripts.js
@@ -0,0 +1,39 @@
+/**
+ * Adds two numbers
+ * @param {Number} num1 The first addend
+ * @param {Number} num2 The second addend
+ * @returns {Number} The sum of num1 + num2
+ */
+ function add(num1, num2) {
+  return num1 + num2;
+}
+
+/**
+ * Subtracts two numbers
+ * @param {Number} num1 The minuend
+ * @param {Number} num2 The subtrahend
+ * @returns {Number} The difference of num1 - num2
+ */
+function subtract(num1, num2) {
+  return num1 - num2;
+}
+
+/**
+ * Multiplies two numbers
+ * @param {Number} num1 The multiplicand
+ * @param {Number} num2 The multiplier
+ * @returns {Number} The product of num1 * num2
+ */
+function multiply(num1, num2) {
+  return num1 * num2;
+}
+
+/**
+ * Divides two numbers
+ * @param {Number} num1 The dividend
+ * @param {Number} num2 The divisor
+ * @returns {Number} The quotient of num1 / num2
+ */
+function divide(num1, num2) {
+  return num1 / num2;
+}
